c2c2cc743c02309fc7fd7e54f29d8f2909c03572
Daniel Tesfai <dannyhd88@gmail.com>
2022-06-15 14:34:05 +0300

    haile

--- app_kin_media/models.py
+++ app_kin_media/models.py
@@ -8,9 +8,6 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-
-
-
 def artist_photo_directory_path(instance ,filename):
     # file will be uploaded to MEDIA_ROOT/artist_id_<id>/<filename>
         return 'artist_id_{0}/{1}'.format(instance.artist_id , filename)
@@ -19,13 +16,9 @@ def album_cover_directory(instance ,filename):
     # file will be uploaded to MEDIA_ROOT/artist_id_<id>/<filename>
         return 'album_id_{0}/{1}'.format(instance.artist_id , filename)
 
-class Artist(models.Model):
 
-    artist_id=models.AutoField(primary_key=True)
-    artist_name=models.CharField(max_length=255 ,default=_(
-    "unknown"),null=False ,blank=False)
-    artist_photo = models.ImageField(upload_to=artist_photo_directory_path, height_field=None, width_field=None, max_length=100)
-    artist_description=models.CharField(max_length=100)
+
+class Artist(models.Model):
 
     class Meta:
         verbose_name = _("Artist")
@@ -33,63 +26,75 @@ class Artist(models.Model):
         ordering = ['artist_id']
 
 
+    artist_id=models.AutoField(primary_key=True)
+    artist_name=models.CharField(max_length=255 ,default=_(
+    "unknown"),null=False ,blank=False)
+    artist_photo = models.ImageField(upload_to=artist_photo_directory_path, height_field=None, width_field=None, max_length=100)
+    artist_description=models.CharField(max_length=100)
+
+
     def __str__(self):
-        return self.artist_name
+       return f'{self.artist_id} {self.artist_name}'
 
 
 
 
 class Album(models.Model):
-
-    Genre= (
-        (0, _('reggea')),
-        (1, _('hip-hop')),
-        (2, _('pop')),
-        (3, _('soul')),
-        (4, _('RnB'))
-    )
-    album_id=models.AutoField(primary_key=True)
-    artist = models.ForeignKey(
-        Artist ,related_name='album_artist_id', default=1 , on_delete=models.DO_NOTHING)
-    album_genre = models.IntegerField(
-        choices=Genre, default=0, verbose_name=_("Genre"))
-
-    album_name=models.CharField(max_length=255 , null=False , blank= False ,default='unknown')
-    album_cover = models.ImageField(upload_to=album_cover_directory, height_field=None, width_field=None, max_length=100)
-    album_description=models.CharField(max_length=255 , null=False , blank=False ,default= "name")
-    album_release_date=models.DateTimeField(auto_now_add=True,)
-
     class Meta:
         verbose_name = _("Album")
         verbose_name_plural = _("Albums")
         ordering = ['album_id']
 
+    Genre= (
+        ("reggea",'reggea'),
+        ("hip-hop",'hip-hop'),
+        ("pop",'pop'),
+        ("solo",'solo'),
+        ("RnB", 'RnB'),
+    )
+    album_id=models.AutoField(primary_key=True)
+    
+    album_genre = models.CharField(max_length=255 ,
+        choices=Genre, default="pop", verbose_name=_("Genre"))
+
+    album_name=models.CharField(max_length=255 , null=False , blank= False ,default='collection')
+    album_cover = models.ImageField(upload_to=album_cover_directory, height_field=None, width_field=None, max_length=100)
+    album_description=models.CharField(max_length=255 , null=False , blank=False ,default= "name")
+    album_release_date=models.DateTimeField(auto_now_add=True,)
+    artist_id = models.ForeignKey(
+        Artist ,related_name='album_artist_id', on_delete=models.DO_NOTHING)
+
+    
+
     def __str__(self):
-        return self.album_name , self.album_release_date , self.album_genre
+           return f'{self.album_id} {self.album_name}'
 
     
 class Track(models.Model):
-
-    track_id=models.AutoField(primary_key=True)
-    artist = models.ForeignKey(
-        Artist , default=1 , on_delete=models.DO_NOTHING)
-    album = models.ForeignKey(
-        Album,related_name='track_album_id', default=1 , on_delete=models.DO_NOTHING)
-    #music_cover = models.ForeignKey(
-      # Album ,related_name='track_album_cover', default=1 , on_delete=models.DO_NOTHING)
-    track_description=models.CharField(max_length=100)
-    track_name= models.CharField(max_length=50 , null=False ,blank=False)
-    track_file = models.FileField(upload_to='' , null=True)
-    duration=DurationField()
 
     class Meta:
         verbose_name = _("Track")
         verbose_name_plural = _("Tracks")
         ordering = ['track_id']
 
+    track_id=models.AutoField(primary_key=True)
+   
+   
+    track_description=models.CharField(max_length=100)
+    track_name= models.CharField(max_length=50 , null=False ,blank=False)
+    track_file = models.FileField(upload_to='' , null=True)
+    duration=DurationField()
 
+    trackalbum_id= models.ForeignKey(
+    Album,related_name='track_album_id', on_delete=models.DO_NOTHING)
+    #trackartist_id= models.ForeignKey(
+    #Artist,related_name='track_artist_id', on_delete=models.DO_NOTHING)
+
+    
+
+  
     def __str__(self):
-        return self.track_name ,self.track_file ,self.track_file
+        return f'{self.track_id} {self.track_name} {self.track_file}'
         
 
 
--- app_kin_media/serializers.py
+++ app_kin_media/serializers.py
@@ -1,35 +1,61 @@
 from rest_framework import serializers
 from .models import Artist , Album , Track
 
+from rest_framework import status
+from rest_framework.response import Response
+
+
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
         fields = "__all__"
 
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+   
+    def perform_create(self, serializer):
+        serializer.save()
+    
+
 class AlbumSerializer(serializers.ModelSerializer):
-    track_album_id = TrackSerializer(many=True)
+    track = TrackSerializer(many=True , required=True)
 
     class Meta:
         model = Album
         fields = "__all__"
 
     def create(self, validated_data):
-        tracks_data = validated_data.pop('track_album_id')
+        # gets our 
+        tracks_data = validated_data.pop('track_id')
         album = Album.objects.create(**validated_data)
         for track_data in tracks_data:
             Track.objects.create(album=album, **track_data)
         return album
 
+    def perform_create(self, serializer):
+        serializer.save()
+
 class ArtistSerializer(serializers.ModelSerializer):
-    album_artist_id = AlbumSerializer(many=True)
+    album = AlbumSerializer(many=True , required=True)
 
     class Meta:
         model = Artist
         fields = "__all__"
 
     def create(self, validated_data):
-        albums_data = validated_data.pop('album_artist_id')
+        # get the album
+        albums_data = validated_data.pop('album_id')
+        print(albums_data)
+        # create our artist based on artist_id serializer
         artist = Artist.objects.create(**validated_data)
         for album_data in albums_data:
             Album.objects.create(artist=artist, **album_data)
-        return artist
+        return artist 
+    def perform_create(self, serializer):
+        serializer.save()
